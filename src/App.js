8439286b0a369a75e88dc252ec7516c896187bd9
Reymond Mesuga <mesugareymondrobin@gmail.com>
2021-04-26 13:04:45 +0800

    Services component havve been added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 // import logo from './logo.svg';
 import Portfolio from './components/Portfolio'
 import './App.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Services from './components/Services';
+
 
 function App() {
     const portfolioLinks = [
@@ -31,93 +35,11 @@ function App() {
 
     return (
         <div className="App">
-            <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-                <div className="container">
-                    <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-                    <button
-                        className="navbar-toggler navbar-toggler-right"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarResponsive"
-                        aria-controls="navbarResponsive"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        Menu
-                        <i className="fa fa-bars"></i>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarResponsive">
-                        <ul className="navbar-nav text-uppercase ml-auto">
-                            <li className="nav-item">
-                                <a className="nav-link js-scroll-trigger" href="#services">Services</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link js-scroll-trigger" href="#about">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link js-scroll-trigger" href="#team">Team</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <Navbar />
 
-            <header className="masthead">
-                <div className="container">
-                    <div className="intro-text">
-                        <div className="intro-lead-in">Welcome To Our Studio!</div>
-                        <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
-                        <a
-                            className="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
-                            href="#services">Tell Me More</a>
-                    </div>
-                </div>
-            </header>
+            <Header />
 
-            <section className="page-section" id="services">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12 text-center">
-                            <h2 className="section-heading text-uppercase">Services</h2>
-                            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                        </div>
-                    </div>
-                    <div className="row text-center">
-                        <div className="col-md-4">
-                            <span className="fa-stack fa-4x">
-                                <i className="fa fa-circle fa-stack-2x text-primary"></i>
-                                <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
-                            </span>
-                            <h4 className="service-heading">E-Commerce</h4>
-                            <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing
-                                elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                        </div>
-                        <div className="col-md-4">
-                            <span className="fa-stack fa-4x">
-                                <i className="fa fa-circle fa-stack-2x text-primary"></i>
-                                <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
-                            </span>
-                            <h4 className="service-heading">Responsive Design</h4>
-                            <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing
-                                elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                        </div>
-                        <div className="col-md-4">
-                            <span className="fa-stack fa-4x">
-                                <i className="fa fa-circle fa-stack-2x text-primary"></i>
-                                <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
-                            </span>
-                            <h4 className="service-heading">Web Security</h4>
-                            <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing
-                                elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <Services />
 
             <Portfolio portfolioLinks={portfolioLinks}></Portfolio>
 
